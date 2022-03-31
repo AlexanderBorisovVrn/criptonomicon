0196e988c76f60229c31b61bde2c6bf1eba54fff
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2022-03-31 22:34:33 +0300

    feat:add tickers filter

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,7 @@
         <div class="flex">
           <div class="max-w-xs">
             <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер</label
-            >
+              >Тикер</label>
             <div class="mt-1 relative rounded-md shadow-md">
               <input
                 @keydown="handler"
@@ -24,7 +23,16 @@
                 type="text"
                 name="wallet"
                 id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                class="
+                  block
+                  w-full
+                  pr-10
+                  border-gray-300
+                  text-gray-900
+                  focus:outline-none focus:ring-gray-500 focus:border-gray-500
+                  sm:text-sm
+                  rounded-md
+                "
                 placeholder="Например DOGE"
               />
             </div>
@@ -40,7 +48,18 @@
                   }
                 "
                 :key="idx"
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                class="
+                  inline-flex
+                  items-center
+                  px-2
+                  m-1
+                  rounded-md
+                  text-xs
+                  font-medium
+                  bg-gray-300
+                  text-gray-800
+                  cursor-pointer
+                "
               >
                 {{ l }}
               </span>
@@ -53,7 +72,28 @@
         <button
           @click="add"
           type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          class="
+            my-4
+            inline-flex
+            items-center
+            py-2
+            px-4
+            border border-transparent
+            shadow-sm
+            text-sm
+            leading-4
+            font-medium
+            rounded-full
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+            transition-colors
+            duration-300
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-gray-500
+          "
         >
           <!-- Heroicon name: solid/mail -->
           <svg
@@ -73,24 +113,77 @@
       </section>
 
       <template v-if="tickers.length">
-          <hr class="w-full border-t border-gray-600 my-4" />
-        <div class="flex items-center ">
-          <label for="filter">Фильтр:
-          <input
-            type="text"
-            name="filter"
-            id="filter"
-            class="pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-          />
+        <hr class="w-full border-t border-gray-600 my-4" />
+        <div class="flex items-center">
+          <label for="filter"
+            >Фильтр:
+            <input
+              v-model="filterTickers"
+              type="text"
+              name="filter"
+              id="filter"
+              class="
+                pr-10
+                border-gray-300
+                text-gray-900
+                focus:outline-none focus:ring-gray-500 focus:border-gray-500
+                sm:text-sm
+                rounded-md
+              "
+            />
           </label>
           <button
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            class="
+              my-4
+              mx-2
+              inline-flex
+              items-center
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              leading-4
+              font-medium
+              rounded-full
+              text-white
+              bg-gray-600
+              hover:bg-gray-700
+              transition-colors
+              duration-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500
+            "
             type="button"
           >
             Назад
           </button>
           <button
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            class="
+              my-4
+              mx-2
+              inline-flex
+              items-center
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              leading-4
+              font-medium
+              rounded-full
+              text-white
+              bg-gray-600
+              hover:bg-gray-700
+              transition-colors
+              duration-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500
+            "
             type="button"
           >
             Вперед
@@ -100,13 +193,20 @@
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t of tickers"
+            v-for="t of filtered()"
             :key="t.name"
             :class="{
               'border-4': selected === t,
             }"
             @click="isSelected(t)"
-            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+            class="
+              bg-white
+              overflow-hidden
+              shadow
+              rounded-lg
+              border-purple-800 border-solid
+              cursor-pointer
+            "
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
@@ -119,7 +219,21 @@
             <div class="w-full border-t border-gray-200"></div>
             <button
               @click.stop="remove(t)"
-              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+              class="
+                flex
+                items-center
+                justify-center
+                font-medium
+                w-full
+                bg-gray-100
+                px-4
+                py-4
+                sm:px-6
+                text-md text-gray-500
+                hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
+                transition-all
+                focus:outline-none
+              "
             >
               <svg
                 class="h-5 w-5"
@@ -195,12 +309,16 @@ export default {
       graph: [],
       autocomplete: [],
       tickersList: [],
+      filterTickers: "",
       isValid: true,
     };
   },
   created() {
-    this.tickers = JSON.parse(localStorage.getItem("tickersList"));
-    this.tickers.forEach((ticker) => this.updatePrice(ticker));
+    const tickersList = JSON.parse(localStorage.getItem("tickersList"));
+    if (tickersList) {
+      this.tickers = tickersList;
+      this.tickers.forEach((ticker) => this.updatePrice(ticker));
+    }
     fetch("https://min-api.cryptocompare.com/data/all/coinlist?summary=true")
       .then((res) => res.json())
       .then((data) => {
@@ -217,9 +335,6 @@ export default {
       })
       .slice(0, 4);
   },
-  beforeUnmount() {
-    console.log("unmounted");
-  },
 
   methods: {
     add() {
@@ -235,7 +350,7 @@ export default {
       }
     },
     updatePrice(ticker) {
-      setInterval(() => {
+      let timer = setInterval(() => {
         fetch(
           `https://min-api.cryptocompare.com/data/price?fsym=${ticker.name}&tsyms=USD&api_key=39dc4489c05f1d652c367141e7d6cbe708692580d58cb897f9bc55e5ca910df9`
         )
@@ -246,7 +361,8 @@ export default {
             if (this.selected?.name === ticker.name) {
               this.graph.push(data.USD);
             }
-          });
+          })
+          .catch((e) => clearInterval(timer));
       }, 3000);
     },
     updateStorage() {
@@ -256,7 +372,6 @@ export default {
       this.tickers = this.tickers.filter((ticker) => {
         return ticker !== itemToRemove;
       });
-      this.updateStorage();
     },
     validateTicker() {
       this.isValid =
@@ -270,6 +385,11 @@ export default {
       return this.graph.map((price) => {
         return 5 + (+(price - minVal) * 95) / (maxVal - minVal);
       });
+    },
+    filtered() {
+      return this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filterTickers.toUpperCase())
+      );
     },
     transformTickerName() {
       return this.ticker.toUpperCase();
