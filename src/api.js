56096b4e9b1f6777add3490472905b6e32c7283f
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2022-04-08 15:31:45 +0300

    refact:multiple request in fetchTickers

--- src/api.js
+++ src/api.js
@@ -1,15 +1,16 @@
 export async function fetchTickers(tickers) {
   const API_KEY = '39dc4489c05f1d652c367141e7d6cbe708692580d58cb897f9bc55e5ca910df9';
-  const _URL = new URL('https://min-api.cryptocompare.com/data/price');
+  const _URL = new URL('https://min-api.cryptocompare.com/data/pricemulti');
   const TICKERS = tickers.join(',');
-  _URL.searchParams.set('fsym', 'USD');
-  _URL.searchParams.set('tsyms', TICKERS);
+  const CURRENCY = 'USD'
+  _URL.searchParams.set('fsyms', TICKERS);
+  _URL.searchParams.set('tsyms', CURRENCY);
   _URL.searchParams.set('api_key', API_KEY);
   console.log(decodeURIComponent(_URL.href));
   return fetch(decodeURIComponent(_URL.href)).then(r => r.json()).then(d => {
     return Object.fromEntries(
       Object.entries(d).map(([key, value]) => {
-        return [key, value = 1 / value]
+        return [key, value = value[CURRENCY]]
       })
     )
   })
