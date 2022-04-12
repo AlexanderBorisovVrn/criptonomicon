8d2592c3247a0bb39e2ac935607864ea90ac45a0
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2022-04-12 22:54:18 +0300

    feat:web sockets fetch data

--- src/api.js
+++ src/api.js
@@ -1,40 +1,57 @@
 const tickersHandlers = new Map();
+const API_KEY = '39dc4489c05f1d652c367141e7d6cbe708692580d58cb897f9bc55e5ca910df9';
+const URLWs = 'wss://streamer.cryptocompare.com/v2';
+const socket = new WebSocket(URLWs + '?api_key=' + API_KEY);
 
-export async function fetchTickers() {
-  if (tickersHandlers.size === 0) {
+socket.addEventListener('message', (e) => {
+  const {
+    TYPE: type,
+    PRICE: newPrice,
+    FROMSYMBOL: currency
+  } = JSON.parse(e.data);
+  const AGREGATE_TYPE = '5';
+  if (type !== AGREGATE_TYPE) {
     return
   }
-  const API_KEY = '39dc4489c05f1d652c367141e7d6cbe708692580d58cb897f9bc55e5ca910df9';
-  const _URL = new URL('https://min-api.cryptocompare.com/data/pricemulti');
-  if (tickersHandlers.size === 0) {
-    return;
-  }
-  const TICKERS = [...tickersHandlers.keys()].join(',');
-  const CURRENCY = 'USD'
-  _URL.searchParams.set('fsyms', TICKERS);
-  _URL.searchParams.set('tsyms', CURRENCY);
-  _URL.searchParams.set('api_key', API_KEY);
+  const handlers = tickersHandlers.get(currency);
+  handlers && handlers.forEach(fn => {
+    fn(newPrice)
+  });
+})
 
-  return fetch(decodeURIComponent(_URL.href)).then(r => r.json()).then(d => {
-    const updatedPrices = Object.fromEntries(
-      Object.entries(d).map(([key, value]) => {
-        return [key, value = value[CURRENCY]]
-      })
-    )
-    Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-      const handlers = tickersHandlers.get(currency) ?? [];
-      handlers.forEach(fn => fn(newPrice))
-    })
+const subscribeAction = "SubAdd";
+const unSubscribeAction = "SubRemove";
+
+function sendMessageToWS(action, tickerName) {
+  return JSON.stringify({
+    "action": action,
+    "subs": [`5~CCCAGG~${tickerName}~USD`]
+  });
+}
+
+
+function subscribeToTickerWs(tickerName) {
+
+  if (socket.readyState === 1) {
+    socket.send(sendMessageToWS(subscribeAction, tickerName));
+    return
+  }
+  socket.addEventListener('open', () => {
+    socket.send(sendMessageToWS(subscribeAction, tickerName))
+  }, {
+    once: true
   })
 }
 
 export function subscribeToTicker(tickerName, cb) {
   const subscribers = tickersHandlers.get(tickerName) || [];
-  tickersHandlers.set(tickerName, [...subscribers, cb])
+  tickersHandlers.set(tickerName, [...subscribers, cb]);
+  subscribeToTickerWs(tickerName)
 }
 
 export function unsubscribe(tickerName) {
- tickersHandlers.delete(tickerName)
+  tickersHandlers.delete(tickerName);
+  socket.send(
+    sendMessageToWS(unSubscribeAction, tickerName)
+  )
 }
-
-setInterval(fetchTickers, 2000)
